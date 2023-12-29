13b99006ea111a4f286027d75c0d440decde5558
Tigran124 <tigran.h.988@gmail.com>
2023-12-29 15:21:37 +0400

    changes completed

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        if (power == 0){
+            System.out.println(1);
+        }else {
+            int num = numberToPrint;
+            for (int i = 1; i < power; i++) {
+                numberToPrint *= num;
+            }
+            System.out.println(numberToPrint);
+        }
     }
 
     public static void main(String[] args) {
